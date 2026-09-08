88392c8edd50551ea39b01bc35027829618b1113
Rosy Yunila <rosyyunila59@gmail.com>
2026-09-08 11:24:52 +0700

    data pribadi

---
+++ praktikum-2/src/DataSaya.java
@@ -0,0 +1,15 @@
+public class DataSaya {
+    public static void main(String[] args) {
+        String nama = "Rosy Yunila";
+        int umur = 19;
+        double tinggi =155;
+        char inisial = 'p';
+        boolean mahasiswaAktif = true;
+
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur);
+        System.out.println("Tinggi Badan: " + tinggi);
+        System.out.println("Inisial: " + inisial);
+        System.out.println("Aktif: " + mahasiswaAktif);
+    }
+}
