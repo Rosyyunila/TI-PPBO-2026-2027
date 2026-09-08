d3d4402a6a2eb322d4810e6d6284cd58254e5a10
Rosy Yunila <rosyyunila59@gmail.com>
2026-09-08 11:10:27 +0700

    biodata saya

---
+++ praktikum-2/src/BiodataSaya.java
@@ -0,0 +1,11 @@
+public class BiodataSaya {
+    public static void main(String[] args) {
+
+        //menampilkan nama
+        System.out.println("Nama: Rosy yunila ");
+        //menampilkan NIM
+        System.out.println("Nim: 2025573010023");
+        //menampilkan nama prodi
+        System.out.println("Prodi: Teknik informatika" );
+           }
+}
