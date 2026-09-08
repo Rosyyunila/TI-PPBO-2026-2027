35c1e3faae4eaa40aae17894c3cc93924ec569dc
Rosy Yunila <rosyyunila59@gmail.com>
2026-09-08 09:44:47 +0700

    Struktur Dasar Program Java

--- praktikum-2/src/Helloworld.java
+++ praktikum-2/src/Helloworld.java
@@ -1,18 +1,11 @@
 public class Helloworld {
     public static void main(String[]args){
-        System.out.println("Halo, Rosy!");
+        System.out.println("Halo, Dunia!");
+
         System.out.print("Nama: ");
         System.out.print("Budi");
         System.out.println();
         System.out.println("Ini baris baru");
-        System.out.println();
-        //program menampilkan biodata sederhana
-        System.out.println("Nama: Budi");
-        /*baris di bawah ini
-        /mencetak NIM mahasiswa
-         */
-        System.out.println("NIM: 12345");
-
 
     }
 }
