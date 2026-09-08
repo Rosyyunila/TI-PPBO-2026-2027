dd6b99b589475b036bb7df8affbefabd1bd549f0
Rosy Yunila <rosyyunila59@gmail.com>
2026-09-08 09:07:15 +0700

    Struktur Dasar Program Java

--- praktikum-2/src/Helloworld.java
+++ praktikum-2/src/Helloworld.java
@@ -1,5 +1,9 @@
 public class Helloworld {
     public static void main(String[]args){
         System.out.println("Halo, Rosy!");
+        System.out.print("Nama: ");
+        System.out.print("Budi");
+        System.out.println();
+        System.out.println("Ini baris baru");
     }
 }
