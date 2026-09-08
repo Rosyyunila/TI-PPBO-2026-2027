adcbc5e497f0755d82e7460b7df432ec1d859c6c
Rosy Yunila <rosyyunila59@gmail.com>
2026-09-08 09:54:07 +0700

    Komentar Kode

--- praktikum-2/src/Helloworld.java
+++ praktikum-2/src/Helloworld.java
@@ -6,6 +6,13 @@ public class Helloworld {
         System.out.print("Budi");
         System.out.println();
         System.out.println("Ini baris baru");
+        System.out.println();
+        //Program menampilkan biodata sederhana
+        System.out.println("Nama: Budi");
+        /*Baris di bawah ini
+         mencetak NIM mahasiswa
+         */
+        System.out.println("NIM: 12345");
 
     }
 }
